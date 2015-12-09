2d6256c4154f0913faa9048c8ff77c7486fa8ba8
Jagdeep Singh <jagdeepkh@gmail.com>
2015-12-09 21:57:17 +0530

    initial commit

---
+++ process.go
@@ -0,0 +1,56 @@
+package ps
+
+import (
+	"fmt"
+	"os/exec"
+	"strconv"
+	"strings"
+)
+
+type Process struct {
+	Pid     int64
+	User    string
+	Cpu     float64
+	Mem     float64
+	Time    string
+	Command string
+}
+
+func compact(collection []string) []string {
+	var result []string
+	for i := 0; i < len(collection); i++ {
+		if collection[i] != "" {
+			result = append(result, collection[i])
+		}
+	}
+	return result
+}
+
+// USER              PID  %CPU %MEM      VSZ    RSS   TT  STAT STARTED      TIME COMMAND
+func Processes() []Process {
+	var result []Process
+	output, _ := exec.Command("ps", "aux").CombinedOutput()
+	process_list := strings.Split(string(output), "\n")
+
+	for i := 1; i < len(process_list); i++ {
+		parts := compact(strings.Split(process_list[i], " "))
+		if len(parts) >= 10 {
+			var process Process
+			process.User = parts[0]
+			process.Pid, _ = strconv.ParseInt(parts[1], 10, 64)
+			process.Cpu, _ = strconv.ParseFloat(parts[2], 64)
+			process.Mem, _ = strconv.ParseFloat(parts[3], 64)
+			process.Time = parts[9]
+			for j := 10; j < len(parts); j++ {
+				process.Command = process.Command + " " + parts[j]
+			}
+			result = append(result, process)
+			fmt.Println(process)
+		}
+	}
+	return process
+}
+
+func main() {
+	Processes()
+}
